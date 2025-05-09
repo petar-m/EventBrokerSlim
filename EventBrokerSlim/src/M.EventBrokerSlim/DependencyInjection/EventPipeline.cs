2b3a4d33832a4693dbda6a96773943e8abfc6f75
petar-m <petar_m@mail.bg>
2025-05-09 12:50:39 +0300

    add missling XML comments

--- EventBrokerSlim/src/M.EventBrokerSlim/DependencyInjection/EventPipeline.cs
+++ EventBrokerSlim/src/M.EventBrokerSlim/DependencyInjection/EventPipeline.cs
@@ -3,4 +3,9 @@ using FuncPipeline;
 
 namespace M.EventBrokerSlim.DependencyInjection;
 
+/// <summary>
+/// Represents a pipeline for handling events.
+/// </summary>
+/// <param name="Event">The type of the event.</param>
+/// <param name="Pipeline">The pipeline to process the event.</param>
 public record EventPipeline(Type Event, IPipeline Pipeline);
