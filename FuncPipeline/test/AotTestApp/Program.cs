f7b5a635c39f6d4d0a681da196d46c67311e924b
petar-m <petar_m@mail.bg>
2025-05-11 16:53:39 +0300

    FuncPipeline AOT test app

---
+++ FuncPipeline/test/AotTestApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using FuncPipeline;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AotTestApp;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var services = new ServiceCollection()
+            .AddSingleton<DateService>()
+            .BuildServiceProvider(true);
+
+        var pipeline = PipelineBuilder.Create(services.GetRequiredService<IServiceScopeFactory>())
+            .NewPipeline()
+            .Execute<string, INext>(async ([ResolveFrom(PrimarySource = Source.Context, Fallback =false, PrimaryNotFound = NotFoundBehavior.ThrowException)]message, next) =>
+            {
+                Console.WriteLine($"Message: {message}");
+                await next.RunAsync();
+                Console.WriteLine($"Completed pipeline.");
+            })
+            .Execute<DateService>(date =>
+            {
+                Console.WriteLine($"Date: {date.GetTime().Result}");
+                return Task.CompletedTask;
+            })
+            .Build()
+            .Pipelines[0];
+
+        var context = new PipelineRunContext().Set<string>("resolved from context");
+        var result = await pipeline.RunAsync(context);
+        Console.WriteLine($"Pipeline result: IsSuccessful = {result.IsSuccessful}");
+
+        result = await pipeline.RunAsync();
+        Console.WriteLine($"Pipeline result: IsSuccessful = {result.IsSuccessful}, Exception = {result.Exception}");
+    }
+}
+
+public class DateService
+{
+     public Task<string> GetTime() => Task.FromResult(DateTime.Now.ToLongDateString());
+}
