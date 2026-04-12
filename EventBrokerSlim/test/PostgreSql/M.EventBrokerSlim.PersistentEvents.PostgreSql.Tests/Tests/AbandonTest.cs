8ca8000fadf7b52115899db24391663afa5b7510
petar-m <petar_m@mail.bg>
2026-04-12 11:50:24 +0300

    add retry and abandon test (postgres)

--- EventBrokerSlim/test/PostgreSql/M.EventBrokerSlim.PersistentEvents.PostgreSql.Tests/Tests/AbandonTest.cs
+++ EventBrokerSlim/test/PostgreSql/M.EventBrokerSlim.PersistentEvents.PostgreSql.Tests/Tests/AbandonTest.cs
@@ -6,13 +6,13 @@ using PostgreSqlIntegrationTests;
 
 namespace M.EventBrokerSlim.PersistentEvents.PostgreSql.Tests.Tests;
 
-public class RetryTest : IDisposable
+public class AbandonTest : IDisposable
 {
     private readonly ServiceProvider _serviceProvider;
     private readonly IServiceScope _scope;
     private readonly Setup _setup;
 
-    public RetryTest(Setup setup)
+    public AbandonTest(Setup setup)
     {
         _setup = setup;
         var builder = PipelineBuilder
@@ -21,9 +21,13 @@ public class RetryTest : IDisposable
             .Execute(async (SampleEvent e, IRetryPolicy retryPolicy, EventReceiver r, EventRecord record) =>
             {
                 r.Add(record);
-                if(record.RetryAttemptCount <= 2)
+                if(record.RetryAttemptCount <= 1)
                 {
                     retryPolicy.RetryAfter(TimeSpan.FromSeconds(1));
+                }
+                else
+                {
+                    retryPolicy.Abandon();
                 }
             })
             .Build();
@@ -33,8 +37,8 @@ public class RetryTest : IDisposable
                 cfg.PollingInterval = TimeSpan.FromSeconds(1);
 
                 db.ConnectionString = setup.ConnectionString;
-                db.Schema = "ebs_5";
-                db.Table = nameof(RetryTest);
+                db.Schema = "ebs_6";
+                db.Table = nameof(AbandonTest);
                 db.CreateEventsTable();
             }))
             .AddEventHandlerPipeline<SampleEvent>(builder.Pipelines[0], o => o.WithHandlerName("sample-event-handler"))
@@ -47,15 +51,15 @@ public class RetryTest : IDisposable
     }
 
     [Fact]
-    public async Task Event_retried_and_successfully_handled()
+    public async Task Event_retried_and_abandoned()
     {
         var broker = _scope.ServiceProvider.GetRequiredService<IEventBroker>();
         var receiver = _scope.ServiceProvider.GetRequiredService<EventReceiver>();
-        var sampleEvent = new SampleEvent("retry");
+        var sampleEvent = new SampleEvent("retry and dead-letter");
 
         await broker.Publish(sampleEvent, TestContext.Current.CancellationToken);
 
-        await receiver.WaitForEventsAsync(4, TimeSpan.FromSeconds(10), TestContext.Current.CancellationToken);
+        await receiver.WaitForEventsAsync(3, TimeSpan.FromSeconds(10), TestContext.Current.CancellationToken);
         var receivedEvents = receiver.GetReceivedEvents();
         var receivedEvent = Assert.Single(receivedEvents, x => x.Event is SampleEvent && x.EventRecord.RetryAttemptCount == 0);
         Assert.Equal(sampleEvent, receivedEvent.Event);
@@ -63,22 +67,20 @@ public class RetryTest : IDisposable
         Assert.Equal(sampleEvent, receivedEvent.Event);
         receivedEvent = Assert.Single(receivedEvents, x => x.Event is SampleEvent && x.EventRecord.RetryAttemptCount == 2);
         Assert.Equal(sampleEvent, receivedEvent.Event);
-        receivedEvent = Assert.Single(receivedEvents, x => x.Event is SampleEvent && x.EventRecord.RetryAttemptCount == 3);
-        Assert.Equal(sampleEvent, receivedEvent.Event);
-        await AssertCompletedStatus(receivedEvent.EventRecord);
+        await AssertDeadLetteredStatus(receivedEvent.EventRecord);
     }
 
-    private async Task AssertCompletedStatus(EventRecord eventRecord)
+    private async Task AssertDeadLetteredStatus(EventRecord eventRecord)
     {
         using var connection = _setup.DataSource.CreateConnection();
         using var command = connection.CreateCommand();
-        command.CommandText = $"SELECT status FROM ebs_5.{nameof(RetryTest)} WHERE id = @id";
+        command.CommandText = $"SELECT status FROM ebs_6.{nameof(AbandonTest)} WHERE id = @id";
         command.Parameters.AddWithValue("id", long.Parse(eventRecord.Id));
         await connection.OpenAsync(TestContext.Current.CancellationToken);
 
         var status = await command.ExecuteScalarAsync(TestContext.Current.CancellationToken);
 
-        Assert.Equal(EventStatus.Completed, (EventStatus)status!);
+        Assert.Equal(EventStatus.DeadLettered, (EventStatus)status!);
     }
 
     public void Dispose()
