ad7eabd49dc2529716588ea26f806dd2e48d5bb1
petar-m <petar_m@mail.bg>
2026-04-12 11:39:52 +0300

    add retry test (postgres)

---
+++ EventBrokerSlim/test/PostgreSql/M.EventBrokerSlim.PersistentEvents.PostgreSql.Tests/Tests/RetryTest.cs
@@ -0,0 +1,90 @@
+﻿using FuncPipeline;
+using M.EventBrokerSlim.DependencyInjection;
+using M.EventBrokerSlim.Persistent;
+using Microsoft.Extensions.DependencyInjection;
+using PostgreSqlIntegrationTests;
+
+namespace M.EventBrokerSlim.PersistentEvents.PostgreSql.Tests.Tests;
+
+public class RetryTest : IDisposable
+{
+    private readonly ServiceProvider _serviceProvider;
+    private readonly IServiceScope _scope;
+    private readonly Setup _setup;
+
+    public RetryTest(Setup setup)
+    {
+        _setup = setup;
+        var builder = PipelineBuilder
+            .Create()
+            .NewPipeline()
+            .Execute(async (SampleEvent e, IRetryPolicy retryPolicy, EventReceiver r, EventRecord record) =>
+            {
+                r.Add(record);
+                if(record.RetryAttemptCount <= 2)
+                {
+                    retryPolicy.RetryAfter(TimeSpan.FromSeconds(1));
+                }
+            })
+            .Build();
+        var services = new ServiceCollection()
+            .AddEventBroker(x => x.WithPostgreSqlPersistence((db, cfg) =>
+            {
+                cfg.PollingInterval = TimeSpan.FromSeconds(1);
+
+                db.ConnectionString = setup.ConnectionString;
+                db.Schema = "ebs_5";
+                db.Table = nameof(RetryTest);
+                db.CreateEventsTable();
+            }))
+            .AddEventHandlerPipeline<SampleEvent>(builder.Pipelines[0], o => o.WithHandlerName("sample-event-handler"))
+            .AddSingleton(EventRegistryHelper.Registry)
+            .AddSingleton<EventReceiver>();
+
+        _serviceProvider = services.BuildServiceProvider();
+        _serviceProvider.UsePersistentEventBroker();
+        _scope = _serviceProvider.CreateScope();
+    }
+
+    [Fact]
+    public async Task Event_retried_and_successfully_handled()
+    {
+        var broker = _scope.ServiceProvider.GetRequiredService<IEventBroker>();
+        var receiver = _scope.ServiceProvider.GetRequiredService<EventReceiver>();
+        var sampleEvent = new SampleEvent("retry");
+
+        await broker.Publish(sampleEvent, TestContext.Current.CancellationToken);
+
+        await receiver.WaitForEventsAsync(4, TimeSpan.FromSeconds(10000000), TestContext.Current.CancellationToken);
+        var receivedEvents = receiver.GetReceivedEvents();
+        var receivedEvent = Assert.Single(receivedEvents, x => x.Event is SampleEvent && x.EventRecord.RetryAttemptCount == 0);
+        Assert.Equal(sampleEvent, receivedEvent.Event);
+        receivedEvent = Assert.Single(receivedEvents, x => x.Event is SampleEvent && x.EventRecord.RetryAttemptCount == 1);
+        Assert.Equal(sampleEvent, receivedEvent.Event);
+        receivedEvent = Assert.Single(receivedEvents, x => x.Event is SampleEvent && x.EventRecord.RetryAttemptCount == 2);
+        Assert.Equal(sampleEvent, receivedEvent.Event);
+        receivedEvent = Assert.Single(receivedEvents, x => x.Event is SampleEvent && x.EventRecord.RetryAttemptCount == 3);
+        Assert.Equal(sampleEvent, receivedEvent.Event);
+        await AssertCompletedStatus(receivedEvent.EventRecord);
+    }
+
+    private async Task AssertCompletedStatus(EventRecord eventRecord)
+    {
+        using var connection = _setup.DataSource.CreateConnection();
+        using var command = connection.CreateCommand();
+        command.CommandText = $"SELECT status FROM ebs_5.{nameof(RetryTest)} WHERE id = @id";
+        command.Parameters.AddWithValue("id", long.Parse(eventRecord.Id));
+        await connection.OpenAsync(TestContext.Current.CancellationToken);
+
+        var status = await command.ExecuteScalarAsync(TestContext.Current.CancellationToken);
+
+        Assert.Equal(EventStatus.Completed, (EventStatus)status!);
+    }
+
+    public void Dispose()
+    {
+        _scope.ServiceProvider.GetRequiredService<IEventBroker>().Shutdown();
+        _scope.Dispose();
+        _serviceProvider.Dispose();
+    }
+}
